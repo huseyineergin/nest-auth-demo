e922b601278711e4121208dfd7f120522bcad5f6
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-03 00:53:29 +0300

    (client): handle unexpected errors

--- client/src/app/(auth)/actions.ts
+++ client/src/app/(auth)/actions.ts
@@ -11,37 +11,45 @@ export async function signInAction(formData: {
 }): Promise<APISuccessResponse<{ accessToken: string }> | APIErrorResponse> {
   const cookieStore = await cookies();
 
-  const res = await fetch(`${baseUrl}/auth/sign-in`, {
-    method: "POST",
-    body: JSON.stringify({
-      username: formData.username,
-      password: formData.password,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    const res = await fetch(`${baseUrl}/auth/sign-in`, {
+      method: "POST",
+      body: JSON.stringify({
+        username: formData.username,
+        password: formData.password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-  if (!res.ok) {
-    try {
-      const error: APIErrorResponse = await res.json();
-      return error;
-    } catch {
-      return {
-        success: false,
-        status: res.status,
-        message: "An unexpected error occurred.",
-      };
+    if (!res.ok) {
+      try {
+        const error: APIErrorResponse = await res.json();
+        return error;
+      } catch {
+        return {
+          success: false,
+          status: res.status,
+          message: "An unexpected error occurred.",
+        };
+      }
     }
+
+    const result: APISuccessResponse<{ accessToken: string }> = await res.json();
+
+    cookieStore.set("accessToken", result.data.accessToken, {
+      path: "/",
+      maxAge: 60 * 15,
+      sameSite: "strict",
+    });
+
+    return result;
+  } catch {
+    return {
+      success: false,
+      status: 503,
+      message: "An unexpected error occurred.",
+    };
   }
-
-  const result: APISuccessResponse<{ accessToken: string }> = await res.json();
-
-  cookieStore.set("accessToken", result.data.accessToken, {
-    path: "/",
-    maxAge: 60 * 15,
-    sameSite: "strict",
-  });
-
-  return result;
 }
 
 export async function signUpAction(formData: {
@@ -50,35 +58,43 @@ export async function signUpAction(formData: {
 }): Promise<APISuccessResponse<{ accessToken: string }> | APIErrorResponse> {
   const cookieStore = await cookies();
 
-  const res = await fetch(`${baseUrl}/auth/sign-up`, {
-    method: "POST",
-    body: JSON.stringify({
-      username: formData.username,
-      password: formData.password,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    const res = await fetch(`${baseUrl}/auth/sign-up`, {
+      method: "POST",
+      body: JSON.stringify({
+        username: formData.username,
+        password: formData.password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-  if (!res.ok) {
-    try {
-      const error: APIErrorResponse = await res.json();
-      return error;
-    } catch {
-      return {
-        success: false,
-        status: res.status,
-        message: "An unexpected error occurred.",
-      };
+    if (!res.ok) {
+      try {
+        const error: APIErrorResponse = await res.json();
+        return error;
+      } catch {
+        return {
+          success: false,
+          status: res.status,
+          message: "An unexpected error occurred.",
+        };
+      }
     }
+
+    const result: APISuccessResponse<{ accessToken: string }> = await res.json();
+
+    cookieStore.set("accessToken", result.data.accessToken, {
+      path: "/",
+      maxAge: 60 * 15,
+      sameSite: "strict",
+    });
+
+    return result;
+  } catch {
+    return {
+      success: false,
+      status: 503,
+      message: "An unexpected error occurred.",
+    };
   }
-
-  const result: APISuccessResponse<{ accessToken: string }> = await res.json();
-
-  cookieStore.set("accessToken", result.data.accessToken, {
-    path: "/",
-    maxAge: 60 * 15,
-    sameSite: "strict",
-  });
-
-  return result;
 }
