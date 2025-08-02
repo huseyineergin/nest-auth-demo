d3786399705c899a919d9531b5e2dfef29bd1ebd
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-02 23:00:54 +0300

    (server): update http exception filter

--- server/src/common/filters/http-exception.filter.ts
+++ server/src/common/filters/http-exception.filter.ts
@@ -5,12 +5,20 @@ import { Response } from "express";
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
+    const exceptionResponse = exception.getResponse();
     const status = exception.getStatus();
+    let message = exception.message;
+
+    if (typeof exceptionResponse === "object" && exceptionResponse !== null) {
+      if ("message" in exceptionResponse) {
+        message = (exceptionResponse as Record<string, string>)["message"];
+      }
+    }
 
     response.status(status).json({
-      message: exception.message,
       success: false,
       status: status,
+      message,
     });
   }
 }
